735b27f9661edb1681b58367a3bee744ca861fd2
Fabien Hermenier <hermenierfabien@gmail.com>
2014-10-22 09:56:26 +0200

    - simplify a bit deletion to remove auto-selection of the previous element. Will be back once this code refactored - fix #10

--- js/sandbox.js
+++ js/sandbox.js
@@ -14,9 +14,6 @@ var endPoint = "http://murmuring-sierra-9341.herokuapp.com";
 var DEFAULT_USE_CASE = "5443ab34d973a61ece00267b";
 var selectedElement = null ;
 var current;
-function registerSelectedElement(element){
-	selectedElement = element;
-}
 
 /**
  * Click to de-select by default
@@ -163,55 +160,18 @@ function onKeyEvent(event){
 			}
 		}
 		// Delete keys : 'd'
-		else if (keyCode == 68) {
-			var newSelectedElement = null ;
-			// If it's a VM select the previous one in the node.
-			if (selectedElement instanceof VirtualMachine){
-				var vm = selectedElement ;
-				var node = config.getHoster(vm.id),
-					vmIndex = node.vms.indexOf(vm);
-				vmIndex++;
-				// Fix vm index
-				var noOver = false;
-				// If no VM over the selected one
-				if (vmIndex >= node.vms.length) {
-					// Target the VM before the selected one
-					vmIndex -= 2 ;
-					noOver = true ;
-				}
-				// If no VM under the selected one
-				if( vmIndex < 0 ){
-					// If there's also no over, select the node
-					if (noOver){
-						newSelectedElement = node ;
-					}
-					// Otherwise, select the one over :
-					vmIndex+=2;
-				}
-				// If this is the last VM of the node, select the node
-				if( node.vms.length == 1){
-					newSelectedElement = node ;
-				}
-				// If there's still some element, get the previous
-				else {
-					newSelectedElement = node.vms[vmIndex];
-				}
-			}
-			selectedElement.delete();
+		else if (keyCode == 68) {					
 			if (selectedElement instanceof Node) {
-				newSelectedElement = null;
+				if (selectedElement.vms.length > 0) {
+					alert("Error: a node must be empty to be turned off")
+				} else {
+					selectedElement.delete();
+					setSelectedElement(null);
+				}
+			} else {
+					selectedElement.delete();
+					setSelectedElement(null);				
 			}			
-			//if (newSelectedElement != null) {
-				setSelectedElement(newSelectedElement);
-			//}
-		}
-		// V key : previous
-		else if (keyCode == 86){
-			shiftSelectedElement(-1);
-		}
-		// B key : next
-		else if (keyCode == 66) {
-			shiftSelectedElement(1);
 		}
 		// O (letter, not zero) key : Switch node state.
 		else if (keyCode == 79) {
